004a49dd524f526b8ebf8d7192042e2896551158
sipamungkas <sipamungkas.dev@gmail.com>
2023-07-13 10:25:11 +0700

    feat: add custom 404 or error page

---
+++ error.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="flex h-screen justify-center items-center flex-col">
+        <h1 class="text-9xl">404</h1>
+        <p class="mt-7 text-4xl text-blue-400">Page not found</p>
+        <button @click="navigateTo('/')" class="text-white mt-9 rounded py-4 px-5 bg-blue-500">
+            Go Back
+        </button>
+    </div>
+</template>
