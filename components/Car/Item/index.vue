8bc6c7af55a9439fd2f5a2203108ab3beacbb941
sipamungkas <sipamungkas.dev@gmail.com>
2023-07-13 14:16:35 +0700

    feat: change img to nuxt-image

--- components/Car/Item/index.vue
+++ components/Car/Item/index.vue
@@ -10,7 +10,7 @@ const props = defineProps({
     <div class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]"
         @click="navigateTo(`/car/${car.name}-${car.id}`)">
         <div class="flex h-full">
-            <img :src="car.url" alt="" class="w-[300px] h-full object-cover" />
+            <NuxtImg :src="car.url" alt="" class="w-[300px] h-full object-cover" />
             <div class="p-4 flex flex-col">
                 <div>
                     <h1 class="text-2xl text-blue-700">{{ car.name }}</h1>
