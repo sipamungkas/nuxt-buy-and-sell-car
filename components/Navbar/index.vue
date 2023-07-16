fd5b92d9ad05ac93b634de905e2ac9fe47e74320
sipamungkas <sipamungkas.dev@gmail.com>
2023-07-16 13:54:12 +0700

    feat(authentication): add login and logout

--- components/Navbar/index.vue
+++ components/Navbar/index.vue
@@ -1,7 +1,37 @@
+<script setup>
+const user = useSupabaseUser()
+
+const supabase = useSupabaseAuthClient();
+
+
+const logout = () => {
+    const { error } = supabase.auth.signOut()
+    if (error) {
+        console.log({ error })
+        throw createError('Failed to logt')
+    }
+    user.value = null
+    navigateTo('/')
+
+}
+
+</script>
+
 <template>
     <div>
         <header class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md">
             <NuxtLink class="text-3xl font-mono" to="/"> Cartrader </NuxtLink>
+            <div v-if="user" class="flex">
+                <NuxtLink class="mr-5" to="/profile/listings">
+                    Profile
+                </NuxtLink>
+                <p class="cursor-pointer" @click="logout">Logout</p>
+            </div>
+            <div v-else>
+                <NuxtLink to="/login">
+                    Login
+                </NuxtLink>
+            </div>
         </header>
     </div>
-</template>
+</template> 
