53a02cc2a749bbbe91c10b6a604fc1d4d06bdd64
sipamungkas <sipamungkas.dev@gmail.com>
2023-07-19 16:39:17 +0700

    feat(server): add get all message by listingid

--- server/api/cars/listings/[listingId]/message.get.ts
+++ server/api/cars/listings/[listingId]/message.get.ts
@@ -1,3 +1,12 @@
-defineEventHandler(async event => {
-    
-})
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default defineEventHandler(async (event) => {
+  const listingId = event.context.params?.listingId;
+  return prisma.message.findMany({
+    where: {
+      listingId,
+    },
+  });
+});
