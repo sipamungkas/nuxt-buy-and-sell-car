c8b8d01f8df726a34bd84d997b772922d4e17fe3
sipamungkas <sipamungkas.dev@gmail.com>
2023-07-19 10:34:21 +0700

    feat(server): add new car create endpoint

---
+++ server/api/cars/listings/index.post.js
@@ -0,0 +1,69 @@
+import Joi from "joi";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const schema = Joi.object().keys({
+  name: Joi.string().required(),
+  model: Joi.string().required(),
+  make: Joi.string().required(),
+  year: Joi.number()
+    .min(1886)
+    .max(new Date().getFullYear() + 1)
+    .required(),
+  miles: Joi.number().required().min(0),
+  city: Joi.string().min(2).required(),
+  numberOfSeats: Joi.number().max(1000).min(1).required(),
+  features: Joi.array().items(Joi.string()).required(),
+  description: Joi.string(),
+  image: Joi.string().required(),
+  listerId: Joi.string().required(),
+  price: Joi.number().min(0).required(),
+});
+
+export default defineEventHandler(async (event) => {
+  const body = await readBody(event);
+
+  const { error, value } = await schema.validate(body);
+
+  if (error) {
+    throw createError({
+      statusCode: 412,
+      message: error.message,
+    });
+  }
+
+  const {
+    name,
+    model,
+    make,
+    year,
+    miles,
+    city,
+    numberOfSeats,
+    features,
+    description,
+    image,
+    listerId,
+    price,
+  } = body;
+
+  const car = await prisma.car.create({
+    data: {
+      name,
+      model,
+      make,
+      year,
+      miles,
+      city,
+      numberOfSeats,
+      features,
+      description,
+      image,
+      listerId,
+      price,
+    },
+  });
+  return car;
+  // return { value, error };
+});
